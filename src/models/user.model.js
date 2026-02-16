f69e509db2e81fa36ea6d010cc17df21c0d0a2aa
Rabi Khatri Chhetri <rabichhetri189@gmail.com>
2026-02-16 09:00:17 +0545

    User Model Created Sucessfully

---
+++ src/models/user.model.js
@@ -0,0 +1,39 @@
+const mongoose=require('mongoose');
+const bcrypt=require('bcryptjs');
+
+const userSchema=new mongoose.Schema({
+    email:{
+        type:String,
+        required:[true,'Email is required for a user'],
+        trim:true,
+        lowercase:true,
+        unique:[true,'Email alrady exists'],
+        match:[/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,'Please fill a valid email address']
+    },
+    name:{
+        type:String,
+        required:[true,'Name is required for creating an account']
+    },
+    password:{
+        type:String,
+        required:[true,'Password is required for creating an account'],
+        minlength:[6,'Password must be at least 6 characters long'],
+        select:false
+    }
+},{timestamps:true})
+
+
+userSchema.pre('save',async function(){
+    if(!this.isModified('password')){
+        return
+    }
+    const hash=await bcrypt.hash(this.password,10)
+    this.password=hash
+    return
+
+})
+userSchema.methods.comparePassword=async function (password) {
+    return await bcrypt.compare(password,this.password)
+}
+const userModel=mongoose.model('User',userSchema);
+module.exports=userModel;
