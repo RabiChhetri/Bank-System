4d710738591aef9ae9b40f441aca0953f6b2d633
Rabi Khatri Chhetri <rabichhetri189@gmail.com>
2026-02-16 13:08:02 +0545

    Account Model Created

---
+++ src/models/account.model.js
@@ -0,0 +1,24 @@
+const mongoose=require('mongoose')
+
+const accountSchema=new mongoose.Schema({
+    user:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'User',
+        required:[true,'Account must be asscoiated with a user']
+    },
+    status:{
+        enum:{
+            values:['ACTIVE','FROZEN','CLOSED'],
+            message:'Status must be either ACTIVE,FROZEN or CLOSED'
+        }
+    },
+    currency:{
+        type:String,
+        required:[true,'Currency is required for creating an account'],
+        default:'INR'
+    }
+},{timestamps:true})
+
+
+const accountModel=mongoose.model('Account',accountSchema)
+module.exports=accountModel
